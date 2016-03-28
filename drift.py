72bed0075705e60dd50b6c6ff5190132bf50053e
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-03-28 14:39:08 -0400

    Update drift.py

--- drift.py
+++ drift.py
@@ -1,1 +1,122 @@
-hi
+"""
+spaceshooter.py
+Author: Daniel Wilson
+Credit: 
+
+Assignment:
+Write and submit a program that implements the spacewar game:
+https://github.com/HHS-IntroProgramming/Spacewar
+"""
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+import math
+SCREEN_WIDTH = 1250
+SCREEN_HEIGHT = 700
+
+
+class SpaceShip(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+        self.vx = 0
+        self.vy = 0
+        self.vr = 0.0
+        self.thrust = 0
+        self.thrustframe = 1
+        SpaceGame.listenKeyEvent("keydown", "w", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "w", self.thrustOff)
+        SpaceGame.listenKeyEvent("keydown", "s", self.UPON)
+        SpaceGame.listenKeyEvent("keyup", "s", self.stop)
+        SpaceGame.listenKeyEvent("keydown", "a", self.Right)
+        SpaceGame.listenKeyEvent("keyup", "a", self.stop)
+        SpaceGame.listenKeyEvent("keydown", "d", self.Left)
+        SpaceGame.listenKeyEvent("keyup", "d", self.stop)
+        self.fxcenter = self.fycenter = 0.5
+
+
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+            self.vx += 0.1*math.cos(self.rotation+1/2*math.pi)
+            self.vy += 0.1*math.sin(self.rotation-1/2*math.pi)
+            if self.thrustframe == 4:
+                self.thrustframe = 1
+            else:
+                self.setImage(0)
+        """if self.vr == .1:
+            self.rotation = self.rotation - 0.00000001
+        if self.vr == -.1:
+            self.rotation = self.rotation + 0.00000001
+        """
+
+    def thrustOn(self, event):
+        self.thrust = 1
+
+    def thrustOff(self, event):
+        self.thrust = 0
+
+    def UPON(self, event):
+        self.vy=1 
+    def DOWNON(self, event):
+        self.vy=-1
+    def Right(self, event):
+        self.vr = .1
+    def Left(self, event):
+        self.vr = -.1
+    def stop(self, event):
+        self.vy=0
+        self.vx=0
+        self.vr=0
+
+
+class SpaceGame(App):
+    """
+    Tutorial4 space game example.
+    """
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = ImageAsset("images/starfield.jpg")
+        bg1 = Sprite(bg_asset, (0,0))
+        bg2 = Sprite(bg_asset, (512,0))
+        bg3 = Sprite(bg_asset, (0, 512))
+        bg4 = Sprite(bg_asset, (512, 512)) 
+        bg5 = Sprite(bg_asset, (1024, 512))
+        bg6 = Sprite(bg_asset, (1024, 0))
+        SpaceShip((100,100))
+        Sun((300,300))
+
+        
+
+    def step(self):
+        for ship in self.getSpritesbyClass(SpaceShip):
+            ship.step()
+
+
+def registerKeys(self, keys):
+        commands = ["left", "right", "forward", "fire"]
+        self.keymap = dict(zip(keys, commands))
+        [self.app.listenKeyEvent("keydown", k, self.controldown) for k in keys]
+        [self.app.listenKeyEvent("keyup", k, self.controlup) for k in keys]
+
+
+class Sun(Sprite):
+    
+    asset5 = ImageAsset("images/sun.png")
+    height = 90
+    width = 40
+    
+    def __init__(self, position):
+        super().__init__(Sun.asset5, position)
+
+myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
